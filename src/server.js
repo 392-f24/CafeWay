ee3647a747517158688c307a9ab818764c096cfd
Mark Fortes <mlgmokus@gmail.com>
2024-10-30 13:27:10 -0500

    added cors for live site

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://cafeway-12c73.web.app'],
     methods: ['GET', 'POST'],
   },
 });
