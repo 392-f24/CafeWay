e4d1085172b6be70c11556d3f3b7b62f4cb90509
Mark Fortes <mlgmokus@gmail.com>
2024-10-30 15:30:59 -0500

    fixed import

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,6 @@ const functions = require("firebase-functions");
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const { database, ref, set, push, onValue } = require("../src/utilities/firebase");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,62 +13,66 @@ const io = new Server(server, {
   },
 });
 
-const reviewsRef = ref(database, "reviews");
-const availabilityRef = ref(database, "availability");
+(async () => {
+  const { database, ref, set, push, onValue } = await import("../src/utilities/firebase.js");
 
-let reviews = [];
-let availability = {};
+  const reviewsRef = ref(database, "reviews");
+  const availabilityRef = ref(database, "availability");
 
-onValue(reviewsRef, (snapshot) => {
-  reviews = snapshot.val() || [];
-  io.emit("updateReviews", reviews);
-});
+  let reviews = [];
+  let availability = {};
 
-onValue(availabilityRef, (snapshot) => {
-  availability = snapshot.val() || {};
-  io.emit("updateAvailability", availability);
-});
-
-io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
-
-  socket.emit("updateReviews", reviews);
-  socket.emit("updateAvailability", availability);
-
-  socket.on("newReview", (review) => {
-    const newReviewRef = push(reviewsRef);
-    set(newReviewRef, review).then(() => {
-    });
+  onValue(reviewsRef, (snapshot) => {
+    reviews = snapshot.val() || [];
+    io.emit("updateReviews", reviews);
   });
 
-  socket.on("newReply", ({ reviewId, text }) => {
-    const reviewIndex = reviews.findIndex((r) => r.id === reviewId);
-    if (reviewIndex !== -1) {
-      reviews[reviewIndex].replies.push(text);
-      set(reviewsRef, reviews).then(() => {
+  onValue(availabilityRef, (snapshot) => {
+    availability = snapshot.val() || {};
+    io.emit("updateAvailability", availability);
+  });
+
+  io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.emit("updateReviews", reviews);
+    socket.emit("updateAvailability", availability);
+
+    socket.on("newReview", (review) => {
+      const newReviewRef = push(reviewsRef);
+      set(newReviewRef, review).then(() => {
       });
-    }
-  });
+    });
 
-  socket.on("updateAvailability", ({ cafeId, seating, outlets }) => {
-    const timestamp = new Date().toLocaleString();
-    const update = { seating, outlets, timestamp };
+    socket.on("newReply", ({ reviewId, text }) => {
+      const reviewIndex = reviews.findIndex((r) => r.id === reviewId);
+      if (reviewIndex !== -1) {
+        reviews[reviewIndex].replies.push(text);
+        set(reviewsRef, reviews).then(() => {
+        });
+      }
+    });
 
-    if (!availability[cafeId]) {
-      availability[cafeId] = { history: [] };
-    }
-    availability[cafeId].history.unshift(update);
-    availability[cafeId].history = availability[cafeId].history.slice(0, 4);
+    socket.on("updateAvailability", ({ cafeId, seating, outlets }) => {
+      const timestamp = new Date().toLocaleString();
+      const update = { seating, outlets, timestamp };
 
-    set(availabilityRef, availability).then(() => {
+      if (!availability[cafeId]) {
+        availability[cafeId] = { history: [] };
+      }
+      availability[cafeId].history.unshift(update);
+      availability[cafeId].history = availability[cafeId].history.slice(0, 4);
+
+      set(availabilityRef, availability).then(() => {
+      });
+    });
+
+    socket.on("disconnect", () => {
+      console.log("A user disconnected:", socket.id);
     });
   });
 
-  socket.on("disconnect", () => {
-    console.log("A user disconnected:", socket.id);
+  exports.api = functions.https.onRequest((req, res) => {
+    server.emit("request", req, res);
   });
-});
-
-exports.api = functions.https.onRequest((req, res) => {
-  server.emit("request", req, res);
-});
+})();
