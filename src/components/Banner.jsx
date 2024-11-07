43f66a076acd1736503e2a3153f0624c4a8fd8e0
angelp03 <angelpimentel2025@u.northwestern.edu>
2024-11-07 16:19:44 -0600

    Add banner component to HomePage

---
+++ src/components/Banner.jsx
@@ -0,0 +1,34 @@
+import './Banner.css';
+import Icon from '../Icon.svg';
+import Search from '../Search.svg';
+
+const Banner = () => {
+    return (
+        <div className='banner-div'>
+            <div className="logo-search-div">
+                <div className='logo-div'>
+                    <img src={Icon}/>
+                    <h1>
+                        CafeWay
+                    </h1>
+                </div>
+                <form>
+                    <input className="text-input" type="text" placeholder="Search for cafes near you" />
+                    <button className="search-btn"> 
+                        <img src={Search}/>
+                    </button>
+                </form>
+            </div>
+            <div className="banner-buttons">
+                <button className="banner-btn">
+                    Review
+                </button>
+                <button className="banner-btn">
+                    Login
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
